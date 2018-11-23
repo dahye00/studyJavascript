7bb029af2e74d29ca14c1be9656a95711bf16a6c
DESKTOP-7IETLFJ\임다혜 <devldh@naver.com>
2018-11-23 11:08:26 +0900

    _each로 for문 줄이기

--- chapter2.js
+++ chapter2.js
@@ -9,6 +9,29 @@ var users = [
   { id: 8, name: 'MP', age: 23},
 ]
 
+/* filter */
+function _filter(list, predi) {//순수함수. 기존배열 그대로. 새로운 필터링된 배열형식 만들기
+    var new_list = [];
+    _each(list, function(val) { if(predi(val)) new_list.push(val);});
+    return new_list;
+}
+
+/* map */
+function _map(list, mapper) {
+    var new_list = [];
+    _each(list, function(val) {new_list.push(mapper(val));});
+    return new_list;
+}
+
+/* each */
+//for문을 좀 더 간결하게 보이도록 하기 위한 함수
+function _each(list, iter) {
+    for(var i = 0; i< list.length; i++) {
+        iter(list[i]);
+    }
+    return list;
+}
+
 /* 명령형 코드로 작성해보기 */
 //1. 30세 이상인 users를 거른다.
 var temp_users = [];
@@ -33,7 +56,7 @@ for(var i = 0; i < users.length; i++) {
         temp_users.push(users[i]);
     }
 }
-console.log("3 : \n" zz temp_users);
+console.log("3 : \n", temp_users);
 //4. 30세 미만인 users의 ages를 수집한다.
 var ages = [];
 for(var i = 0; i < users.length; i++) {
@@ -41,27 +64,9 @@ for(var i = 0; i < users.length; i++) {
         ages.push(users[i].age);
     }
 }
-console.log("4 :"ages);
+console.log("4 :", ages);
 
 //_filter, _map으로 리팩토링
-
-/* filter */
-function _filter(list, predi) {//순수함수. 기존배열 그대로. 새로운 필터링된 배열형식 만들기
-    var new_list = [];
-    for(var i = 0; i < list.length; i++) {
-        if(predi(list[i])) new_list.push(list[i]);
-    }
-    return new_list;
-}
-
-/* map */
-function _map(list, mapper) {
-    var new_list = [];
-    for(var i = 0; i < list.length; i++) {
-        new_list.push(mapper(list[i]));
-    }
-    return new_list;
-}
 
 //30세 이상인 users를 거른다.
 console.log(_filter(users, function(user){return user.age >= 30 }));
@@ -73,3 +78,16 @@ console.log(_map(over_30, function(user){return user.name}));
 //30세 미만인 users의 ages를 거른다.
 var under_30 = _filter(users, function(user){return user.age < 30 });
 console.log(_map(under_30, function(user){return user.age}));
+
+//더 간결하게 표현하기
+//30세 이상인 users의 names를 거른다.
+console.log(
+    _filter(users, function(user){return user.age >= 30 }),
+    _map(over_30, function(user){return user.name})
+);
+//30세 미만인 users의 ages를 거른다.
+console.log(
+    _filter(users, function(user){return user.age < 30 }),
+    _map(over_30, function(user){return user.age})
+);
+console.clear();
